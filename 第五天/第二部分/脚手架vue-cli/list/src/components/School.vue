ce2354b176fc5bfbb018763d0be94d8a8dffb4ad
洪超 <8746124+kin0926@user.noreply.gitee.com>
2022-08-19 23:47:39 +0800

    2022.8.19 插件

--- 第五天/第二部分/脚手架vue-cli/list/src/components/School.vue
+++ 第五天/第二部分/脚手架vue-cli/list/src/components/School.vue
@@ -2,13 +2,11 @@
   <!-- 快捷键输入 <vueInit -->
   <div>
     <h2>学校地址{{ address }}</h2>
-    <h2 @click="showName">学校名称{{ name }}</h2>
+    <h2>学校名称{{ name }}</h2>
   </div>
 </template>
 
 <script>
-// 调用mixin，再在下面使用
-import hunhe from '../hunhe.js'
 
 export default {
   name: "school",
@@ -18,8 +16,6 @@ export default {
       name: "设计院",
     };
   },
-  // 使用多个时用，隔开
-  mixins:[hunhe]
 }
 </script>
 
--- 第五天/第二部分/脚手架vue-cli/list/src/hunhe.js
+++
@@ -1,7 +0,0 @@
-export default{
-    methods: {
-        showName(){
-            alert(this.name)
-        }
-    }
-}

869ba175e218918350a974b96a317be69d5eb517
洪超 <8746124+kin0926@user.noreply.gitee.com>
2022-09-07 13:00:16 +0800

    2022.9.7使用外部样式库animate.css导入动画效果   1、先用npm安装样式   2、在用import导入库   3、最后在transition里使用样式

---
+++ 第五天/第二部分/脚手架vue-cli/list/0.8动画效果/components/Demo1.vue
@@ -0,0 +1,49 @@
+<template lang="">
+    <div>
+        <button @click="isShow = !isShow">显示/隐藏</button>
+        <!-- transition过渡（页面加载时会自动脱掉这个标签）,appear刷新后直接先进场动画 -->
+        <transition name="hello" appear>
+            <h1 v-show="isShow">彩蛋</h1>
+        </transition>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Demo1',
+    data() {
+        return {
+            isShow:true
+        }
+    },
+}
+</script>
+
+<style scoped>
+    h1{
+        background-color: orange;
+    }
+
+    /* 进入动画，v-enter-active(vue,进入，激活) */
+    /* 这里的hello是上面<transition name="hello">里的name ，如果上面没写name，那这里就用v，即(vue) */
+    .hello-enter-active{
+        /* 播放动画下面的aa，播放1s */
+        animation: aa 1s;
+    }
+    /* 离开动画 */
+    .hello-leave-active{
+        /* reverse反转 */
+        animation: aa 1s reverse;
+    }
+
+    /* 动画关键帧 */
+    @keyframes aa{
+        from{
+            transform: translateX(-100%);
+        }
+        to{
+            transform: translateX(0px);
+        }
+    }
+</style>
---
+++ 第五天/第二部分/脚手架vue-cli/list/0.9外部样式库导入 动画效果/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div>
+    <Demo1Vue></Demo1Vue>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Demo1Vue from './components/Demo1.vue';
+
+export default {
+  name: "App",
+  components: {Demo1Vue},
+};
+</script>

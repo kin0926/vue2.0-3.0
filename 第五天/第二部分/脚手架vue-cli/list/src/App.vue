4b8cb57afe84e2bccce20f0fa24b28df34659ae1
洪超 <8746124+kin0926@user.noreply.gitee.com>
2022-09-16 16:54:02 +0800

    1默认插槽 在App.vue里的xx组件里写内容，再到xx组件里html里写<slot></slot> 内容在解析完毕后会被放到xx组件里的slot所在位置 slot里可以写内容，作为默认值，当没有内容传递给slot时默认显示

--- 第五天/第二部分/脚手架vue-cli/list/src/App.vue
+++ 第五天/第二部分/脚手架vue-cli/list/src/App.vue
@@ -1,18 +1,35 @@
 <template lang="">
-  <div class="container">
-    <SearchVue/>
-    <ListVue/>
+  <div>
+    <CategoryVue title="游戏">
+      <img
+        src="https://cdn.pixabay.com/photo/2022/07/21/13/40/mongolian-girl-7336271_960_720.jpg"
+        alt=""
+      />
+    </CategoryVue>
+
+    <CategoryVue title="美食">
+      <ul>
+        <li v-for="(i, index) in foods" :key="index">{{ i }}</li>
+      </ul>
+    </CategoryVue>
+
+    <CategoryVue title="汽车"></CategoryVue>
   </div>
 </template>
 
 <script>
-  // 引入
-import SearchVue from './components/Search';
-import ListVue from './components/List';
-
+import CategoryVue from "./components/Category.vue";
 export default {
   name: "App",
-  // 注册
-  components:{SearchVue,ListVue}
+  data() {
+    return {
+      games: ["崩坏1", "崩坏2", "原神", "崩坏3"],
+      foods: ["火锅", "奶茶", "小龙虾", "意面"],
+      cars: ["宝马", "丰田", "大众", "火焰鸟"],
+    };
+  },
+  components: { CategoryVue },
 };
 </script>
+
+<style lang=""></style>

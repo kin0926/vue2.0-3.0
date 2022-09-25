adb8a03e94fdef62ebddb97abb01d569a07e3b7a
洪超 <8746124+kin0926@user.noreply.gitee.com>
2022-09-25 15:59:54 +0800

    1.33求和案例-vuex版本

---
+++ 第五天/第二部分/脚手架vue-cli/list/1.33求和案例-vuex版本/App.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div>
+    <CountVue/>
+</div>
+</template>
+
+<script>
+import CountVue from './components/Count.vue';
+export default {
+  name: "App",
+  
+  components: { CountVue },
+};
+</script>
+
+<style lang=""></style>

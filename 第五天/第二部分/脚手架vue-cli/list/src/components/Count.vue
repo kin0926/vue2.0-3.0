b4dc903de8ba4717dd1dd60470413775cb0a83e1
洪超 <8746124+kin0926@user.noreply.gitee.com>
2022-09-27 11:11:53 +0800

    mapState,mapGetters,mapMutations,mapActions四种方法的学习

--- 第五天/第二部分/脚手架vue-cli/list/src/components/Count.vue
+++ 第五天/第二部分/脚手架vue-cli/list/src/components/Count.vue
@@ -10,16 +10,16 @@
       <option value="2">2</option>
       <option value="3">3</option>
     </select>
-    <button @click="add">+</button>
-    <button @click="subtract">-</button>
-    <button @click="multiply">*</button>
-    <button @click="addWait">等一等再加</button>
+    <button @click="add(n)">+</button>
+    <button @click="subtract(n)">-</button>
+    <button @click="multiply(n)">*</button>
+    <button @click="addWait(n)">等一等再加</button>
   </div>
 </template>
 
 <script>
 // 为了批量添加computed里的那些，先导入mapState【map映射 state状态】
-import { mapState } from "vuex";
+import { mapState,mapGetters,mapMutations,mapActions } from "vuex";
 export default {
   name: "Count",
   data() {
@@ -41,30 +41,45 @@ export default {
   
     // 用mapState生成$store.state，从state中读取数据
     // ...xxx,意思是把xxx内容展开放到...所在位置
-    // ...mapState({sum:'sum',school:'school',subject:'subject'}),
-    
+         //对象写法
+    // ...mapState({sum:'sum',school:'school',subject:'subject'}), 
+         //数组写法
     // 如果两个名字一样，可以用数组形式，要加''，
     // 如果要简写不能用{}显示，因为sum{}会自动解析为sum:sum，而不是sum:'sum'
     ...mapState(['sum','school','subject']),
+
+    // $store.getters也是相同方法
   },
 
   methods: {
-    add() {
-      this.$store.dispatch("add", this.n);
-    },
     // 因为减没有业务逻辑，比如判断什么的，所以直接commit发送给mutations也可以
-    subtract() {
-      this.$store.commit("SUBTRACT", this.n);
-    },
-    multiply() {
-      this.$store.dispatch("multiply", this.n);
-    },
+    // subtract() {
+    //   this.$store.commit("SUBTRACT", this.n);
+    // },
+    // 用vuex的mapMutations生成方法时因为没有传入n值所以要在html里的相关@click里传入参数（n）
+    ...mapMutations({subtract:'SUBTRACT'}),     //对象写法
+    // 数组写法
+    // ...mapMutations(['SUBTRACT']),                //数组写法，名字相同时
+    
+    
+    // add() {
+    //   this.$store.dispatch("add", this.n);
+    // },
+    // multiply() {
+    //   this.$store.dispatch("multiply", this.n);
+    // },
+    // 有定时器这种逻辑运算的无法使用下面的vuex的mapActions方法生成
     addWait() {
       // 定时器也可以写在store的actions里
       setTimeout(() => {
         this.$store.dispatch("add", this.n);
       }, 800);
     },
+    // 用vuex的mapActions
+    // 对象方法
+    // ...mapActions({add:'add',multiply:'multiply'}),
+    // 数组方法
+    ...mapActions(['add','multiply'])
   },
 };
 </script>
